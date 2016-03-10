a11f3fce53a310da4c04f18ddcb614af66b0fa85
vidya <vsagarj@gmail.com>
2016-03-10 10:03:16 -0800

    Add routing for 'root'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :headlines
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  # root 'headlines#index'
+  root 'headlines#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -16,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :headlines
 
   # Example resource route with options:
   #   resources :products do
