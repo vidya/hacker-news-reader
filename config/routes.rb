13f60fe620ddab022b015cd2edef7754cb3a1695
vidya <vsagarj@gmail.com>
2016-03-09 21:43:28 -0800

    Move the 'headlines' resource to the top of the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :headlines
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,7 +16,6 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-    resources :headlines
 
   # Example resource route with options:
   #   resources :products do
